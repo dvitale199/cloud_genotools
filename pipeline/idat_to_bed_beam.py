233805cedc125fceff7a8763ee73710ccaefe9da
dvitale199 <d.vitale199@gmail.com>
2024-10-14 20:39:54 -0600

    minimal local print test

--- pipeline/idat_to_bed_beam.py
+++ pipeline/idat_to_bed_beam.py
@@ -1,19 +1,44 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import subprocess
-import argparse
+import logging
+
+class IaapOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--barcodes_file',
+            required=True,
+            help='GCS path to the barcodes text file.'
+        )
+        parser.add_argument(
+            '--iaap_cli',
+            required=True,
+            help='Path to the iaap-cli executable.'
+        )
+        parser.add_argument(
+            '--input_base_path',
+            required=True,
+            help='Base GCS path to the input idats.'
+        )
+        parser.add_argument(
+            '--out_path',
+            required=True,
+            help='GCS path to the output directory.'
+        )
+        parser.add_argument(
+            '--bpm',
+            required=True,
+            help='GCS path to the bpm file.'
+        )
+        parser.add_argument(
+            '--egt',
+            required=True,
+            help='GCS path to the egt file.'
+        )
 
 class RunIaapCliDoFn(beam.DoFn):
     def __init__(self, iaap_cli, input_base_path, out_path, bpm, egt):
-        """Initialize the DoFn with the necessary parameters.
-
-        Args:
-            iaap_cli (str): Path to the iaap-cli executable.
-            input_base_path (str): Base GCS path to the input idats.
-            out_path (str): GCS path to the output directory.
-            bpm (str): GCS path to the bpm file.
-            egt (str): GCS path to the egt file.
-        """
         self.iaap_cli = iaap_cli
         self.input_base_path = input_base_path
         self.out_path = out_path
@@ -21,15 +46,7 @@ class RunIaapCliDoFn(beam.DoFn):
         self.egt = egt
 
     def process(self, barcode):
-        """Runs iaap-cli command for the given barcode.
-
-        Args:
-            barcode (str): The barcode to process.
-
-        Yields:
-            str: A message indicating success or failure.
-        """
-        barcode = barcode.strip()  # remove any leading/trailing whitespace
+        barcode = barcode.strip()
         input_path = f'{self.input_base_path}/{barcode}'
         
         iaap_command = [
@@ -43,64 +60,38 @@ class RunIaapCliDoFn(beam.DoFn):
             '-t', '4'
         ]
 
-        print(f"Executing command: {' '.join(iaap_command)}")
+        logging.info(f"Executing command: {' '.join(iaap_command)}")
+        print(iaap_command)
         # try:
         #     subprocess.run(iaap_command, check=True)
         #     yield f'Barcode {barcode}: Processing completed successfully.'
         # except subprocess.CalledProcessError as e:
         #     error_msg = f'Barcode {barcode}: Error during processing.\n{e}'
+        #     logging.error(error_msg)
         #     yield error_msg
 
-def run_pipeline(known_args, pipeline_args=None):
-    options = PipelineOptions(pipeline_args)
-    with beam.Pipeline(options=options) as pipeline:
+def run_pipeline(pipeline_options):
+    iaap_options = pipeline_options.view_as(IaapOptions)
+    with beam.Pipeline(options=pipeline_options) as pipeline:
         (
             pipeline
-            | 'ReadBarcodes' >> beam.io.ReadFromText(known_args.barcodes_file)
+            | 'ReadBarcodes' >> beam.io.ReadFromText(iaap_options.barcodes_file)
             | 'RunIaapCli' >> beam.ParDo(
                 RunIaapCliDoFn(
-                    iaap_cli=known_args.iaap_cli,
-                    input_base_path=known_args.input_base_path,
-                    out_path=known_args.out_path,
-                    bpm=known_args.bpm,
-                    egt=known_args.egt
+                    iaap_cli=iaap_options.iaap_cli,
+                    input_base_path=iaap_options.input_base_path,
+                    out_path=iaap_options.out_path,
+                    bpm=iaap_options.bpm,
+                    egt=iaap_options.egt
                 )
             )
             | 'PrintResults' >> beam.Map(print)
         )
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--barcodes_file',
-        required=True,
-        help='GCS path to the barcodes text file.'
-    )
-    parser.add_argument(
-        '--iaap_cli',
-        required=True,
-        help='Path to the iaap-cli executable.'
-    )
-    parser.add_argument(
-        '--input_base_path',
-        required=True,
-        help='Base GCS path to the input idats.'
-    )
-    parser.add_argument(
-        '--out_path',
-        required=True,
-        help='GCS path to the output directory.'
-    )
-    parser.add_argument(
-        '--bpm',
-        required=True,
-        help='GCS path to the bpm file.'
-    )
-    parser.add_argument(
-        '--egt',
-        required=True,
-        help='GCS path to the egt file.'
-    )
-    known_args, pipeline_args = parser.parse_known_args()
+    logging.getLogger().setLevel(logging.INFO)
 
-    run_pipeline(known_args, pipeline_args)
+    pipeline_options = PipelineOptions()
+    iaap_options = pipeline_options.view_as(IaapOptions)
+
+    run_pipeline(pipeline_options)
--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 docker buildx build --platform linux/amd64 -t iaap-cli-beam:latest --load --no-cache .
 
-docker run --platform linux/amd64 -it --rm --entrypoint /bin/bash iaap-cli-beam:latest
+# for local testing
+docker run --platform linux/amd64 -it -v $(pwd):/app --rm --entrypoint /bin/bash iaap-cli-beam:latest
+
+python /app/pipeline/idat_to_bed_beam.py \
+  --barcodes_file /app/data/idats.txt \
+  --iaap_cli /app/executables/iaap-cli/iaap-cli \
+  --input_base_path gs://my-bucket/idats \
+  --out_path gs://my-bucket/output \
+  --bpm gs://my-bucket/resources/my_file.bpm \
+  --egt gs://my-bucket/resources/my_file.egt
+#   --runner DirectRunner
